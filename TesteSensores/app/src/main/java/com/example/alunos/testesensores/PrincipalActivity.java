7247364bde655d7ae02039bb520cbd90e1071b8e
Jéssica Carvalho <63794913+jessiwellen@users.noreply.github.com>
2020-08-13 15:41:13 -0300

    Add files via upload

---
+++ TesteSensores/app/src/main/java/com/example/alunos/testesensores/PrincipalActivity.java
@@ -0,0 +1,63 @@
+package com.example.alunos.testesensores;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PrincipalActivity extends AppCompatActivity {
+    Button btnListar, btnAvancar;
+    TextView txtSensores;
+    public static SensorManager mSensores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        mSensores = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        btnListar = findViewById(R.id.btnListar);
+        btnAvancar =findViewById(R.id.btnAvancar);
+        txtSensores = findViewById(R.id.txtSensores);
+        txtSensores.setMovementMethod(new ScrollingMovementMethod());
+
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnAvancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+        private void proximaJanela() {
+        Intent janela = new Intent(this, SensoresActivity.class);
+        startActivity(janela);
+        }
+
+        private void listarSensores(){
+        List<Sensor> listaSensores = mSensores.getSensorList(Sensor.TYPE_ALL);
+        String descobertos = "TOTAL DE SENSORES: " +listaSensores.size()+ "\n\n";
+
+        for (Sensor s: listaSensores){
+            descobertos += s.getName() + "\n";
+        }
+        txtSensores.setText(descobertos);
+
+
+        }
+
+}
